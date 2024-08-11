bb4eb0e90417f8484f78d17d16e3c53489e682c1
AminPainterPro <freelance.aminpainter786@gmail.com>
2024-08-11 13:35:48 +0530

    prettier

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,33 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import React, { FC } from 'react';
+import { Link, HeadFC, PageProps } from 'gatsby';
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import { Badge } from 'shadcn/badge';
+import { Typography } from 'shadcn/typography';
+import { Button } from 'shadcn/button';
+import { Icon } from 'shadcn/icon';
+import SiteSeo from 'components/seo';
+import bearImg from 'images/404.webp';
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const NotFoundPage: FC<PageProps> = () => (
+  <section className='container py-16 grid grid-cols-2 gap-x-20 gap-y-10 lg:grid-cols-none min-h-[90vh] items-center'>
+    <div className='lg:order-last'>
+      <img src={bearImg} alt='BEAR' className='lg:w-3/4 lg:mx-auto' />
+    </div>
 
-const NotFoundPage: React.FC<PageProps> = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+    <div className='space-y-4'>
+      <Badge variant='outline'>Page not found</Badge>
+      <Typography variant='primary'>OOPS! Error 404</Typography>
+      <p className='lg:w-2/3'>
+        Maybe our pet bear broke this page. Allow us to work on it. Meanwhile, you can safely return
+        to the home page.
       </p>
-    </main>
-  )
-}
+      <Link to='/' className='block'>
+        <Button startIcon={<Icon name='Home' />}>Take me home</Button>
+      </Link>
+    </div>
+  </section>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <SiteSeo title='Not Found' />;
